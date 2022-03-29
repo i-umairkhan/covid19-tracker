1c1b353362aabed45e88e82bc602b8bd355e8ca6
Umair Khan <umair.k2607@gmail.com>
2022-03-29 21:24:45 +0500

    Created Dropdown and made api call for countries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import logo from './logo.svg';
+import { FormControl, MenuItem, Select } from '@mui/material';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries,setCountries] = useState([])
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then(reponse => reponse.json())
+      .then(data => {
+        const countries = data.map(country => ({
+          name : country.country,
+          value : country.countryInfo.iso2
+        }))
+        setCountries(countries);
+      });
+    }
+    getCountriesData();
+  },[])
+
+
   return (
     <div className="app">
+      <div className='app__header'>
+      <h1>Covid-19 Tracker</h1>
+      <FormControl className='app__dropdown'>
+        <Select variant='outlined' value='abc'>
+          {countries.map(country => {
+            return(
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            )
+          })}
+        </Select>
+      </FormControl>
+      </div>
     </div>
   );
 }
